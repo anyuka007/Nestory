a00542183a65783271cf0a67ed9619978f0bbc31
anyuka007 <anna.popova.de@gmail.com>
2024-10-21 17:20:50 +0200

    add connection credentials

--- backend/lib/db.js
+++ backend/lib/db.js
@@ -2,8 +2,13 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
+const mongoUser = process.env.MONGO_USER;
+const mongoPassword = process.env.MONGO_PASSWORD;
+const mongoCluster = process.env.MONGO_CLUSTER;
+const mongoDatabase = process.env.MONGO_DATABASE;
+
 async function connect() {
-    const uri = process.env.MONGODB_URI;
+    const uri = `mongodb+srv://${mongoUser}:${mongoPassword}@${mongoCluster}/${mongoDatabase}?retryWrites=true&w=majority`;
     try {
         await mongoose.connect(uri);
         console.log(
