d707a244871de39f4e61e48a91430159253db25c
anyuka007 <anna.popova.de@gmail.com>
2024-10-22 22:40:08 +0200

    Add WishlistItem component and update Wishlist page layout

---
+++ frontend/src/components/WishlistItem/WishlistItem.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const WishlistItem = ({ wishItem }) => {
+    return (
+        <div className="w-full h-[55rem] flex justify-around ">
+            <div className="basis-[23%] flex justify-center items-center">
+                <img
+                    src={wishItem.imgUrl}
+                    alt="wish item photo"
+                    className="w-[20rem] h-[20rem]"
+                />
+            </div>
+            <div className="basis-[47%] flex flex-col justify-center">
+                <p className="pt-[1rem]">{wishItem.name}</p>
+                {/* <p>{wishItem._id}</p> */}
+                <p>{"⭐".repeat(wishItem.rating)}</p>
+                {<p>{wishItem.description}</p>}
+                <p className="my-[2rem]">
+                    <Link to={`/product/${wishItem._id}`}>Details</Link>
+                </p>
+            </div>
+            <div className="basis-[23%] flex flex-col justify-center ">
+                <p className="line-through">{wishItem.price.toFixed(2)}€</p>
+                <p className="text-[3rem] text-[#FFB128]">
+                    {Math.round(wishItem.price * 0.8).toFixed(2)}€
+                </p>
+                <p className="text-green-500">Is available</p>
+                <p className="text-green-500">Free shipping</p>
+                <button className="border">Add to cart</button>
+            </div>
+        </div>
+    );
+};
+
+export default WishlistItem;
