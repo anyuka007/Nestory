e1dac5a66107437158c9c7ab88447330ca915d2e
anyuka007 <anna.popova.de@gmail.com>
2024-10-24 16:59:06 +0200

    Complete responsive design

--- frontend/src/components/WishlistItem/WishlistItem.jsx
+++ frontend/src/components/WishlistItem/WishlistItem.jsx
@@ -6,45 +6,51 @@ import { Trash2 } from "lucide-react";
 
 const WishlistItem = ({ wishItem, deleteWishItem, addToCart }) => {
     return (
-        <div className="h-fit md:h-[28rem] py-[3rem] flex flex-col lg:flex-row justify-around border-b">
-            <div className="md:basis-[23%] flex justify-between md:justify-center">
+        <div className="h-fit md:h-[30rem] py-[3rem] flex flex-col md:flex-row justify-around border-b">
+            <div className="md:basis-[30%] flex md:flex-col justify-between md:justify-center md:mx-8">
                 <div className="basis-[15%]">
                     {wishItem.discount > 0 && (
-                        <p className="text-white text-center bg-colorTertiary w-[5rem]">
+                        <p
+                            className={`text-white text-center ${
+                                wishItem.discount > 40
+                                    ? "bg-colorTertiary"
+                                    : "bg-colorSecondary"
+                            } w-[5rem]`}
+                        >
                             -{wishItem.discount}%
                         </p>
                     )}
                 </div>
                 <div>
-                    <Link to="to={`/product/${wishItem._id}`}">
+                    <Link to={`/product/${wishItem._id}`}>
                         <img
                             src={wishItem.imgUrl}
                             alt="wish item photo"
-                            className="w-[20rem] h-[20rem]"
+                            className="w-[20rem] h-[20rem] mx-auto"
                         />
                     </Link>
                 </div>
-                <div className=" text-center basis-[15%]">
+                <div className="text-center basis-[15%] flex justify-center">
                     <button
                         onClick={deleteWishItem}
-                        className="text-colorPrimary md:hidden"
+                        className=" h-8 w-8 text-colorPrimary md:hidden mb-auto"
                     >
                         <Trash2 />
                     </button>
                 </div>
             </div>
-            <div className="basis-[47%] flex flex-col justify-center">
-                <p className="pt-[1rem]">{wishItem.name}</p>
+            <div className="basis-[47%] flex flex-col justify-center md:mx-8">
+                <p className="pt-8 md:pt-0">{wishItem.name}</p>
                 <StarRating rate={wishItem.rating} />
                 <p className="pt-8">{wishItem.description}</p>
-                <p className="my-[2rem]">
+                <p className="mt-auto pt-4">
                     <Link to={`/product/${wishItem._id}`} className="underline">
                         Details
                     </Link>
                 </p>
             </div>
-            <div className="basis-[20%] flex flex-col justify-center ">
-                <div className="flex justify-between">
+            <div className="basis-[18%] flex flex-col justify-center md:mx-8">
+                <div className="flex justify-between mt-8 md:mt-0">
                     {wishItem.discount ? (
                         <div>
                             <p className="line-through text-[2rem]">
@@ -65,18 +71,18 @@ const WishlistItem = ({ wishItem, deleteWishItem, addToCart }) => {
                     )}
                     <button
                         onClick={deleteWishItem}
-                        className="text-colorPrimary hidden md:block"
+                        className="p-4 text-colorPrimary hidden md:block mb-auto"
                     >
                         <Trash2 />
                     </button>
                 </div>
 
-                <p className="text-green-500">Is available</p>
-                <p className="text-green-500">Free shipping</p>
-                <div className="flex justify-center mt-4">
+                <p className="text-green-600">Is available</p>
+                <p className="text-green-600">Free shipping</p>
+                <div className="flex justify-center mt-auto pt-8">
                     <Button
                         text="Add to cart"
-                        width="250px"
+                        width="200px"
                         onClickHandler={addToCart}
                     />
                 </div>
