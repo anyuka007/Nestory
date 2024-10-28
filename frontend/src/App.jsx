6b5c9b24c8626cedf9a4f4d9994d5f7f6ff26d23
anyuka007 <anna.popova.de@gmail.com>
2024-10-28 10:18:27 +0100

    Refactor user profile routes to use relative paths

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -27,7 +27,7 @@ const App = () => (
                     <Route path="wishlist" element={<Wishlist />} />
                     <Route path="login" element={<Login />} />
                     <Route path="register" element={<Register />} />
-                    <Route path="/user" element={<UserProfile />}>
+                    <Route path="user" element={<UserProfile />}>
                         <Route path="orders" element={<UserOrders />} />
                         <Route path="data" element={<UserProfileInfo />} />
                     </Route>
