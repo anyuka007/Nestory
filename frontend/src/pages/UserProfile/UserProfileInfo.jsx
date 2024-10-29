3aa01cf1cc2ab55a4ddda54b2e4205914c987ed9
anyuka007 <anna.popova.de@gmail.com>
2024-10-29 16:53:34 +0100

    start implement UserProfileInfo component and add dropdown functionality

--- frontend/src/pages/UserProfile/UserProfileInfo.jsx
+++ frontend/src/pages/UserProfile/UserProfileInfo.jsx
@@ -1,9 +1,90 @@
 import React from "react";
+import { useState } from "react";
+import { ChevronDown } from "lucide-react";
 
 const UserProfileInfo = () => {
+    const [dropdowns, setDropdowns] = useState({
+        dropdown1: false,
+        dropdown2: false,
+        dropdown3: false,
+    });
+
+    const dropDownHandler = (id) => {
+        setDropdowns((prevState) => ({
+            ...prevState,
+            [id]: !prevState[id],
+        }));
+    };
     return (
-        <div className="flex justify-center items-center h-full">
-            <h1>Page: UserProfileInfo</h1>
+        <div className="flex flex-col h-full">
+            <h2 className="text-center text-[3rem] md:text-[4rem] lg:[4.8rem] mb-[1rem]">
+                Your profile
+            </h2>
+            <div>
+                <div>
+                    <div className="flex justify-between">
+                        <p>Your personal data</p>
+                        <div className="flex gap-2">
+                            <p>Show details</p>
+                            <button
+                                id="dropdown1"
+                                onClick={() => dropDownHandler("dropdown1")}
+                            >
+                                <ChevronDown />
+                            </button>
+                        </div>
+                    </div>
+                    <div
+                        className={`${
+                            !dropdowns.dropdown1 ? "hidden" : ""
+                        } text-red-500 w-24 h-24`}
+                    >
+                        TEST
+                    </div>
+                </div>
+                <div>
+                    <div className="flex justify-between">
+                        <p>Your access data</p>
+                        <div className="flex gap-2">
+                            <p>Show details</p>
+                            <button
+                                id="dropdown2"
+                                onClick={() => dropDownHandler("dropdown2")}
+                            >
+                                <ChevronDown />
+                            </button>
+                        </div>
+                    </div>
+                    <div
+                        className={`${
+                            !dropdowns.dropdown2 ? "hidden" : ""
+                        } text-red-500 w-24 h-24`}
+                    >
+                        TEST
+                    </div>
+                </div>
+                <div>
+                    <div className="flex justify-between">
+                        <p>Your address</p>
+                        <div className="flex gap-2">
+                            <p>Show details</p>
+                            <button
+                                id="dropdown3"
+                                onClick={() => dropDownHandler("dropdown3")}
+                            >
+                                <ChevronDown />
+                            </button>
+                        </div>
+                    </div>
+                    <div
+                        className={`${
+                            !dropdowns.dropdown3 ? "hidden" : ""
+                        } text-red-500 w-24 h-24`}
+                    >
+                        TEST
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
