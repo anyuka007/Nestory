6534b7212504289dc814c88444b1ca07297cf042
JianLu <jian.lu@dci-student.org>
2024-10-29 09:06:45 +0100

    chore: Update page layouts and add PagesBanner component to ShopList page

--- frontend/src/pages/ShopList/ShopList.jsx
+++ frontend/src/pages/ShopList/ShopList.jsx
@@ -1,5 +1,11 @@
+import PagesBanner from "../../components/PagesBanner/PagesBanner";
+
 const ShopList = () => {
-    return <div>ShopList page</div>;
+    return (
+        <div>
+            <PagesBanner title={"SHOP"} />
+        </div>
+    );
 };
 
 export default ShopList;
