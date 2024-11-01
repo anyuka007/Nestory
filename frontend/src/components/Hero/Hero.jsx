3d217cf3e5e50e7219d33a60a295ffd064670e6a
JianLu <jian.lu@dci-student.org>
2024-11-01 07:35:18 +0100

    refactor: Remove unused code and comments from Hero and Timer components

--- frontend/src/components/Hero/Hero.jsx
+++ frontend/src/components/Hero/Hero.jsx
@@ -1,14 +1,14 @@
-import Button from "../Button/Button";
-import StarRating from "../StarRating/StarRating";
-import ProductCard from "../ProductCard/ProductCard";
+// import Button from "../Button/Button";
+// import StarRating from "../StarRating/StarRating";
+// import ProductCard from "../ProductCard/ProductCard";
 
-const product = {
-    name: "Product Name",
-    image: "/images/beds/bed1.webp",
-    rate: 4.5,
-    percentage: "70%",
-    price: 560,
-};
+// const product = {
+//     name: "Product Name",
+//     image: "/images/beds/bed1.webp",
+//     rate: 4.5,
+//     percentage: "70%",
+//     price: 560,
+// };
 const Hero = () => {
     return (
         <div className="mx-auto w-full aspect-[4/2] bg-colorPrimary rounded-3xl relative">
