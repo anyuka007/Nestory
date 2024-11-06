c1c833e9b4cef8126ac4242721c7893eba481e48
JianLu <jian.lu@dci-student.org>
2024-11-06 13:24:47 +0100

    Fix import path for authController and improve formatting in authorization middleware

--- backend/middleware.js/auth.js
+++ backend/middleware.js/auth.js
@@ -1,27 +1,29 @@
-import { verifyJwt } from "../controllers/authController.js";
+import { verifyJwt } from "../controllers/authControllers.js";
 import dotenv from "dotenv";
 dotenv.config();
 
 //ovaa funkcija ima uloga samo da go zacuva user id
 
 export const authorize = (req, res, next) => {
-  const token = req.cookies.jwt; //go zimam nadvor od cookie kutija
+    const token = req.cookies.jwt; //go zimam nadvor od cookie kutija
 
-  if (!token) {
-    return res.status(401).json({ message: "Unauthorized, token not found" });
-  }
+    if (!token) {
+        return res
+            .status(401)
+            .json({ message: "Unauthorized, token not found" });
+    }
 
-  try {
-    const decodedToken = verifyJwt(token);
-    //se zacuvuva vo req.user, ne kako do sega req.body
+    try {
+        const decodedToken = verifyJwt(token);
+        //se zacuvuva vo req.user, ne kako do sega req.body
 
-    req.user = decodedToken; // attach user to request object
-  } catch (error) {
-    console.log("Token verification failed:", error);
-    return res
-      .status(401)
-      .json({ message: "Unauthorized, verification fails" });
-  }
+        req.user = decodedToken; // attach user to request object
+    } catch (error) {
+        console.log("Token verification failed:", error);
+        return res
+            .status(401)
+            .json({ message: "Unauthorized, verification fails" });
+    }
 
-  next();
+    next();
 };
