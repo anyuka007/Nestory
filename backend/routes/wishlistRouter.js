4b5b56dc4ef47e76cbfb4cd7176ace6303ed2095
anyuka007 <anna.popova.de@gmail.com>
2024-11-06 13:29:49 +0100

    refactor: Update wishlistRouter to include authorization middleware

--- backend/routes/wishlistRouter.js
+++ backend/routes/wishlistRouter.js
@@ -5,11 +5,15 @@ import {
     getAllWishlists,
     getUserWishlist,
 } from "../controllers/wishlist/wishlist.js";
+import { authorize } from "../middleware.js/auth.js";
 
 const wishlistRouter = express.Router();
 
-wishlistRouter.route("/").get(getUserWishlist);
-wishlistRouter.route("/all").get(getAllWishlists);
-wishlistRouter.route("/:id").post(addWishlistItem).delete(deleteWishlistItem);
+wishlistRouter.route("/").get(authorize, getUserWishlist);
+wishlistRouter.route("/all").get(authorize, getAllWishlists);
+wishlistRouter
+    .route("/:id")
+    .post(authorize, addWishlistItem)
+    .delete(authorize, deleteWishlistItem);
 
 export default wishlistRouter;
