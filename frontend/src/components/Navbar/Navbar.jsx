be1f82ec91d7d135bbb682fcf9e3312abfcb9810
JianLu <jian.lu@dci-student.org>
2024-11-07 17:32:46 +0100

    Refactor Navbar and AppProvider components for improved context usage and state management

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -11,16 +11,19 @@ import { Link } from "react-router-dom";
 import HamburgMenu from "../HamburgMenu/HamburgMenu";
 import CategoryMenu from "../CategoryMenu/CategoryMenu";
 import "../../css/App.css";
-import { useState, useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AppContext } from "../../context/AppProvider";
 
 const Navbar = () => {
     const [search, setSearch] = useState(false);
-  const { cartCount } = useContext(AppContext);
-  console.log("nav", cartCount);
 
-    const { loginSuccess, searchKeyword, setSearchKeyword, wishlist } =
-        useContext(AppContext);
+    const {
+        loginSuccess,
+        searchKeyword,
+        setSearchKeyword,
+        wishlist,
+        cartCount,
+    } = useContext(AppContext);
     const [clickUser, setClickUser] = useState(false);
 
     const handleSearch = (e) => {
@@ -28,48 +31,49 @@ const Navbar = () => {
         console.log("searchKeyword", searchKeyword);
     };
 
-  return (
-    <div className="px-20 md:px-20 lg:px-40 xl:px-80 w-full h-[6rem] xl:h-[11rem] flex flex-col bg-white fixed z-20 shadow-sm shadow-gray-200  ">
-      <div className="h-full xl:h-[60%] flex justify-between w-full ">
-        {/* MOBILE */}
+    return (
+        <div className="px-20 md:px-20 lg:px-40 xl:px-80 w-full h-[6rem] xl:h-[11rem] flex flex-col bg-white fixed z-20 shadow-sm shadow-gray-200  ">
+            <div className="h-full xl:h-[60%] flex justify-between w-full ">
+                {/* MOBILE */}
 
-        <div className="w-full flex justify-between items-center md:hidden relative">
-          <div>
-            <Link
-              to={"/"}
-              onClick={() => {
-                window.scrollTo(0, 0);
-              }}
-            >
-              neSTory
-            </Link>
-          </div>
-          <div className="flex justify-between items-center gap-2 ">
-            <HamburgMenu />
-            {!search && <Search onClick={() => setSearch(!search)} />}
-          </div>
-          {search && (
-            <div className="w-full flex absolute top-28 left-0 justify-center items-center gap-2 z-10 bg-white">
-              <input
-                type="text"
-                placeholder="Search"
-                className=" bg-slate-100 rounded-md w-[80%]  h-[4rem] focus:outline-none relative"
-              />
-              <Link
-                className="absolute right-[7rem]"
-                to={"/search"}
-                onClick={() => {
-                  window.scrollTo(0, 0);
-                }}
-              >
-                <Search />
-              </Link>
-              <X
-                onClick={() => setSearch(!search)}
-                className="cursor-pointer transition-transform duration-300
-                    ease-in-out transform hover:rotate-180 text-gray-400
-                    hover:text-gray-900"
-
+                <div className="w-full flex justify-between items-center md:hidden relative">
+                    <div>
+                        <Link
+                            to={"/"}
+                            onClick={() => {
+                                window.scrollTo(0, 0);
+                            }}
+                        >
+                            neSTory
+                        </Link>
+                    </div>
+                    <div className="flex justify-between items-center gap-2 ">
+                        <HamburgMenu />
+                        {!search && (
+                            <Search onClick={() => setSearch(!search)} />
+                        )}
+                    </div>
+                    {search && (
+                        <div className="w-full flex absolute top-28 left-0 justify-center items-center gap-2 z-10 bg-white">
+                            <input
+                                type="text"
+                                placeholder="Search"
+                                className=" bg-slate-100 rounded-md w-[80%]  h-[4rem] focus:outline-none relative"
+                            />
+                            <Link
+                                className="absolute right-[7rem]"
+                                to={"/search"}
+                                onClick={() => {
+                                    window.scrollTo(0, 0);
+                                }}
+                            >
+                                <Search />
+                            </Link>
+                            <X
+                                onClick={() => setSearch(!search)}
+                                className="cursor-pointer transition-transform duration-300
+                  ease-in-out transform hover:rotate-180 text-gray-400
+                  hover:text-gray-900"
                             />
                         </div>
                     )}
@@ -243,71 +247,13 @@ const Navbar = () => {
                         <HamburgMenu className="xl:hidden" />
                     </div>
                 </div>
+            </div>
 
+            <div className="hidden xl:h-[40%] xl:flex  w-full">
+                <CategoryMenu />
             </div>
-            <div className="flex justify-between items-center gap-2">
-              <input
-                type="text"
-                placeholder="Search"
-                className=" bg-slate-100 rounded-md px-2 py-1 md:w-[30rem] lg:w-[40rem] xl:w-[58rem] h-[4rem] focus:outline-none"
-              />
-
-              <Link
-                to={"/search"}
-                onClick={() => {
-                  window.scrollTo(0, 0);
-                }}
-              >
-                <Search />
-              </Link>
-            </div>
-          </div>
-          {/* RIGHT */}
-          <div className="w-1/3 flex justify-end items-center gap-16">
-            <div className="relative">
-              <Link
-                to={"/cart"}
-                onClick={() => {
-                  window.scrollTo(0, 0);
-                }}
-              >
-                {/* julijana */}
-                {/* <span className="number">0</span> */}
-                <span className="number">{cartCount}</span>
-                <ShoppingBag size={26} />
-              </Link>
-            </div>
-            <div className="relative">
-              <Link
-                to={"/wishlist"}
-                onClick={() => {
-                  window.scrollTo(0, 0);
-                }}
-              >
-                <span className="number">3</span>
-                <Heart size={26} />
-              </Link>
-            </div>
-            <div>
-              <Link
-                to={"/login"}
-                onClick={() => {
-                  window.scrollTo(0, 0);
-                }}
-              >
-                <User size={26} />
-              </Link>
-            </div>
-            <HamburgMenu className="xl:hidden" />
-          </div>
         </div>
-      </div>
-
-      <div className="hidden xl:h-[40%] xl:flex  w-full">
-        <CategoryMenu />
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Navbar;
