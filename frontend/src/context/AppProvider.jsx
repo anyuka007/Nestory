7e0a50906abce1d6a5bb094dab69dd2b32a269f4
julijana <julijana3uneva@gmail.com>
2024-11-12 12:01:34 +0100

    logout-jwt-löschen

--- frontend/src/context/AppProvider.jsx
+++ frontend/src/context/AppProvider.jsx
@@ -8,119 +8,119 @@ import { useNavigate } from "react-router-dom";
 export const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-    const [sortOption, setSortOption] = useState("default");
-    const [cartCount, setCartCount] = useState(0);
-    const [loginSuccess, setLoginSuccess] = useState(false);
-    const [cartItems, setCartItems] = useState([]);
-    const [wishlist, setWishlist] = useState([]);
-    const [searchKeyword, setSearchKeyword] = useState("");
-    const [heartCount, setHeartCount] = useState(0);
-    const [user, setUser] = useState({});
-    const [product, setProduct] = useState({});
+  const [sortOption, setSortOption] = useState("default");
+  const [cartCount, setCartCount] = useState(0);
+  const [loginSuccess, setLoginSuccess] = useState(false);
+  const [cartItems, setCartItems] = useState([]);
+  const [wishlist, setWishlist] = useState([]);
+  const [searchKeyword, setSearchKeyword] = useState("");
+  const [heartCount, setHeartCount] = useState(0);
+  const [user, setUser] = useState({});
+  const [product, setProduct] = useState({});
 
-    //fly to shopping cart
-    const [isFlying, setIsFlying] = useState(false);
-    const [flyStyle, setFlyStyle] = useState({});
-    const bagIconRef = useRef(null);
-    const imgRef = useRef(null);
+  //fly to shopping cart
+  const [isFlying, setIsFlying] = useState(false);
+  const [flyStyle, setFlyStyle] = useState({});
+  const bagIconRef = useRef(null);
+  const imgRef = useRef(null);
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const fetchUser = async () => {
-        const res = await fetch("http://localhost:3000/api/checkUser", {
-            credentials: "include",
-        });
+  const fetchUser = async () => {
+    const res = await fetch("http://localhost:3000/api/checkUser", {
+      credentials: "include",
+    });
 
-        if (!res.ok) {
-            setUser({});
-            setLoginSuccess(false);
-            navigate("/");
-            return;
-        }
-        const data = await res.json();
-        console.log("fetchUser", data);
-        setUser(data.user);
-        setLoginSuccess(data.success);
+    if (!res.ok) {
+      setUser({});
+      setLoginSuccess(false);
+      navigate("/");
+      return;
+    }
+    const data = await res.json();
+    console.log("fetchUser", data);
+    setUser(data.user);
+    setLoginSuccess(data.success);
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
+  useEffect(() => {
+    const getWishlist = async () => {
+      const items = await fetchWishlist();
+      setWishlist(items);
     };
+    if (user._id) {
+      getWishlist();
+    }
+  }, [user._id]);
 
-    useEffect(() => {
-        fetchUser();
-    }, []);
+  //julijana
+  const resetUserData = () => {
+    setUser({});
+    setCartItems([]);
+    setWishlist([]);
+    setCartCount(0);
+    setHeartCount(0);
+    setLoginSuccess(false);
+  };
 
-    useEffect(() => {
-        const getWishlist = async () => {
-            const items = await fetchWishlist();
-            setWishlist(items);
-        };
-        if (user._id) {
-            getWishlist();
-        }
-    }, [user._id]);
+  const handleLogout = async () => {
+    try {
+      console.log(555);
+      await fetch("http://localhost:3000/api/users/logout", {
+        method: "POST",
+        credentials: "include",
+      });
 
-    //julijana
-    const resetUserData = () => {
-        setUser({});
-        setCartItems([]);
-        setWishlist([]);
-        setCartCount(0);
-        setHeartCount(0);
-        setLoginSuccess(false);
-    };
+      console.log("Logout successful");
+      resetUserData();
+      //   navigate("/login");
+      navigate("/");
+    } catch (error) {
+      console.error("Logout error:", error);
+    }
+  };
+  //-----bis hier
 
-    const handleLogout = async () => {
-        try {
-            console.log(555);
-            await fetch("/api/logout", {
-                method: "POST",
-                credentials: "include",
-            });
+  return (
+    <AppContext.Provider
+      value={{
+        sortOption,
+        setSortOption,
+        cartCount,
+        setCartCount,
+        wishlist,
+        setWishlist,
+        loginSuccess,
+        setLoginSuccess,
+        searchKeyword,
+        setSearchKeyword,
+        heartCount,
+        setHeartCount,
+        user,
+        setUser,
+        cartItems,
+        setCartItems,
+        product,
+        setProduct,
+        resetUserData,
+        handleLogout,
 
-            console.log("Logout successful");
-            resetUserData();
-            //   navigate("/login");
-            navigate("/");
-        } catch (error) {
-            console.error("Logout error:", error);
-        }
-    };
-    //-----bis hier
-
-    return (
-        <AppContext.Provider
-            value={{
-                sortOption,
-                setSortOption,
-                cartCount,
-                setCartCount,
-                wishlist,
-                setWishlist,
-                loginSuccess,
-                setLoginSuccess,
-                searchKeyword,
-                setSearchKeyword,
-                heartCount,
-                setHeartCount,
-                user,
-                setUser,
-                cartItems,
-                setCartItems,
-                product,
-                setProduct,
-                resetUserData,
-                handleLogout,
-
-                //fly animation
-                isFlying,
-                setIsFlying,
-                flyStyle,
-                setFlyStyle,
-                bagIconRef,
-                imgRef,
-            }}
-        >
-            {children}
-        </AppContext.Provider>
-    );
+        //fly animation
+        isFlying,
+        setIsFlying,
+        flyStyle,
+        setFlyStyle,
+        bagIconRef,
+        imgRef,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
 };
 
 export default AppProvider;
