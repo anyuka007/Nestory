2f075bb67ef6a942a1390b1071dc3e9e006fe9fa
julijana <julijana3uneva@gmail.com>
2024-11-13 15:22:06 +0100

    logo

--- frontend/src/components/Footer/DividerLogo.jsx
+++ frontend/src/components/Footer/DividerLogo.jsx
@@ -1,22 +1,22 @@
 import React from "react";
 
 export default function DividerLogo() {
-    return (
-        <div className=" mx-[10%]">
-            <div className="flex items-center justify-center my-8">
-                <div className="flex-grow border-t border-gray-300" ></div>
+  return (
+    <div className=" mx-[10%]">
+      <div className="flex items-center justify-center my-8">
+        <div className="flex-grow border-t border-gray-300"></div>
 
-                <div className="mx-4 text-center">
-                    {/* Dein zentrales Symbol oder Bild */}
-                    <img
-                        src="https://themes.muffingroup.com/be/furniturestore2/wp-content/uploads/2023/02/befurniturestore2-footer-logo.svg"
-                        alt="Logo"
-                        className="h-42 w-42 rounded-full"
-                    />
-                </div>
-
-                <div className="flex-grow border-t border-gray-300"></div>
-            </div>
+        <div className="mx-4 text-center">
+          {/* Dein zentrales Symbol oder Bild */}
+          <img
+            src="/images/logo/nestoryFull.png"
+            alt="Logo"
+            className="h-auto w-40 rounded-full "
+          />
         </div>
-    );
+
+        <div className="flex-grow border-t border-gray-300"></div>
+      </div>
+    </div>
+  );
 }
