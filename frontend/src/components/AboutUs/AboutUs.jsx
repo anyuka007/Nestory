a9216c72ec3fc42f292d923834cef9a80dc6c9fd
Alex <mohammad.mohammad@dci-student.org>
2024-11-13 11:43:28 +0100

    stash--

---
+++ frontend/src/components/AboutUs/AboutUs.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function AboutUs() {
+  return (<>
+    <div>
+        <h1>Salan</h1>
+      <section className=''>
+        <div><img src="" alt="" /></div>
+        <div>Ali</div>
+      </section>
+      <section></section>
+      <section></section>
+      <section></section>
+    </div></>
+  )
+}
