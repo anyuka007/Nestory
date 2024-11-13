b673760f336fd88f80681c39516b36a091765a5c
julijana <julijana3uneva@gmail.com>
2024-11-13 11:13:21 +0100

    refresh-cookie-backend

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 import { get } from "mongoose";
 dotenv.config();
 
-const { JWT_SECRET } = process.env;
+const { JWT_SECRET, JWT_REFRESH_SECRET } = process.env;
 
 export function verifyJwt(token) {
   if (!token) return;
@@ -29,7 +29,7 @@ function isStrongPassword(password) {
 
 export const registerUser = async (req, res) => {
   console.log(222);
-  const { firstName, lastName, email, password } = req.body;
+  const { firstName, lastName, email, password, rememberMe } = req.body;
   console.log(req.body);
   try {
     if (!firstName || !lastName || !email || !password) {
@@ -69,15 +69,32 @@ export const registerUser = async (req, res) => {
 
     await user.save();
 
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-      expiresIn: "5h",
-    });
+    const token = jwt.sign(
+      { id: user._id, email: user.email },
+      process.env.JWT_SECRET,
+      // {
+      //   expiresIn: rememberMe ? "5h" : "1h",
+      // }
+      { expiresIn: rememberMe ? "5d" : "1h" }
+    );
 
-    //   const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-    //     expiresIn: "15d",
-    //   });
+    const refreshToken = jwt.sign(
+      { id: user._id },
+      process.env.JWT_REFRESH_SECRET,
+      {
+        expiresIn: rememberMe ? "5d" : "1h",
+      }
+    );
 
     res.cookie("jwt", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      maxAge: 1 * 60 * 60 * 1000,
+      sameSite: "strict",
+    });
+
+    // res.cookie("rememberMe", rememberMe, {
+    res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
       maxAge: 5 * 60 * 60 * 1000,
@@ -88,7 +105,16 @@ export const registerUser = async (req, res) => {
     //dali ovde vo ovoj res treba da dadam pveke podatoci za userot?
     //token ne e potreben, vaka koga se zacuvuva vo local storage
     //od tuka go brisam token
-    res.status(201).json({ message: "User registered" });
+    res.status(201).json({
+      message: "User registered",
+      token,
+      user: {
+        id: user._id,
+        email: user.email,
+        firstName: user.firstName,
+        lastName: user.lastName,
+      },
+    });
   } catch (error) {
     console.error("Login error:", error);
     return res.status(500).json({ message: "Internal server error" });
@@ -97,7 +123,7 @@ export const registerUser = async (req, res) => {
 
 export const loginUser = async (req, res) => {
   console.log(111);
-  const { email, password } = req.body;
+  const { email, password, rememberMe } = req.body;
   console.log(req.body);
   try {
     // Проверка дали корисникот постои , ovoj user ima so wie so id nummer
@@ -116,7 +142,15 @@ export const loginUser = async (req, res) => {
       { id: user._id, email: user.email },
       process.env.JWT_SECRET,
       {
-        expiresIn: "5h",
+        expiresIn: rememberMe ? "5d" : "1h",
+      }
+    );
+
+    const refreshToken = jwt.sign(
+      { id: user._id, email: user.email },
+      process.env.JWT_REFRESH_SECRET,
+      {
+        expiresIn: rememberMe ? "5d" : "1h",
       }
     );
 
@@ -124,6 +158,13 @@ export const loginUser = async (req, res) => {
     //   const refreshToken = createRefreshToken(user._id);
 
     res.cookie("jwt", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      maxAge: 1 * 60 * 60 * 1000,
+      sameSite: "strict",
+    });
+
+    res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
       maxAge: 5 * 60 * 60 * 1000,
@@ -145,9 +186,42 @@ export const loginUser = async (req, res) => {
   }
 };
 
+export const refreshToken = async (req, res) => {
+  const { refreshToken } = req.cookies;
+
+  if (!refreshToken) {
+    return res.status(401).json({ message: "No refresh token provided" });
+  }
+
+  try {
+    const decoded = jwt.verify(refreshToken, JWT_REFRESH_SECRET);
+    const newAccessToken = createAccessToken(decoded.id);
+
+    res.cookie("jwt", newAccessToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      maxAge: 1 * 60 * 60 * 1000,
+      sameSite: "strict",
+    });
+
+    console.log("Token refreshed successfully");
+    res.status(200).json({ newAccessToken });
+  } catch (error) {
+    console.error("Token refresh error:", error);
+    res.status(403).json({ message: "Invalid refresh token" });
+  }
+};
+
 export const logoutUser = async (req, res) => {
   try {
     res.clearCookie("jwt", {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      path: "/",
+      sameSite: "strict",
+    });
+
+    res.clearCookie("refreshToken", {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
       path: "/",
